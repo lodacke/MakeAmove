e4a425760aff61f850057c1f358befccb0420ca8
Grace <yintingchancytg@gmail.com>
2023-04-24 14:49:41 +0200

    Created header of the frontpage

---
+++ frontpage/frontpage.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const bodyDom = document.querySelector("body");
+const headerDom = document.createElement("header");
+const mainDom = document.querySelector("main");
+
+bodyDom.insertBefore(headerDom, mainDom);
+
+headerDom.innerHTML = `
+  <img class=logo src="image/logo.png" alt="appLogo">
+  <h1>Make A Move</h1>
+`;

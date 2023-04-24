e8f8c4eefb0b62f700991b23fa5dd5c87825a8a2
Grace <yintingchancytg@gmail.com>
2023-04-24 14:51:25 +0200

    Created two buttons

--- frontpage/frontpage.js
+++ frontpage/frontpage.js
@@ -10,3 +10,8 @@ headerDom.innerHTML = `
   <img class=logo src="image/logo.png" alt="appLogo">
   <h1>Make A Move</h1>
 `;
+
+mainDom.innerHTML = `
+  <button>Sign In</button>
+  <button>Sign Up</button>
+`;

a7558028055fac548bf54ec21ce9ca7294fc74f7
Grace <yintingchancytg@gmail.com>
2023-04-25 16:27:00 +0200

    Added submitLogin function

--- js/login.js
+++ js/login.js
@@ -1,6 +1,38 @@
 "use strict";
 
-function renderloginPage(event) {
+async function submitLogin(event) {
+  event.preventDefault();
+
+  let message = document.querySelector("main .message");
+
+  try {
+    let response = await fetch("../api/login.php", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.elements.email.value,
+        password: this.elements.password.value,
+      }),
+    });
+
+    let data = await response.json();
+
+    if (!response.ok) {
+      message.innerHTML = `Oops! Something went wrong, we got this from the server <span>${data.message}</span>.`;
+    } else {
+      delete data.password;
+
+      window.localStorage.setItem("user", JSON.stringify(data));
+      renderDatingPage();
+    }
+  } catch (err) {
+    message.textContent = `Error: ${err.message}`;
+  }
+}
+
+function renderLoginPage() {
+  let bodyDom = document.querySelector("body");
+
   bodyDom.innerHTML = `
     <header>
       <img class="logo" src="image/logo.png" alt="appLogo">
@@ -8,12 +40,13 @@ function renderloginPage(event) {
     </header>
     <main>
       <h2>Login</h2>
-      <form class="login-form">
-        <input type="text" class="email" placeholder="Email">
-        <input type="password" class="password" placeholder="Password">
+      <form class="login-form" action="login.php" method="POST">
+        <input type="text" class="email" name="email" placeholder="Email">
+        <input type="password" class="password" name="password" placeholder="Password">
         <button class="login-button" type="submit">Login</button>
-        <img class="loading" src="image/spinner.svg" alt="loading">
-      </form>
+        <p class="message"></p>` +
+        // TODO: <img class="loading" src="image/spinner.svg" alt="loading">
+    `</form>
       <p class="login-kind-words">You're not alone! Many like-minded people are looking for their love from Make A Move. Let us help you! 🫰🏼</p>
       <button class="go-to-register">Register</button>
     </main>
@@ -21,37 +54,8 @@ function renderloginPage(event) {
 
   const goToRegister = document.querySelector(".go-to-register");
   goToRegister.addEventListener("click", renderRegisterPage);
-}
 
-async function loginWithAccount() {
-  try {
-    let response = await fetch("api/login.php", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username: user.username,
-        password: user.password,
-      }),
-    });
-
-    let data = await response.json();
-
-    // If we didn't manage to login we'll redirect the user to our
-    // login page
-    if (!response.ok) {
-      renderLoginPage();
-    } else {
-      // Otherwise we expect to receive the user
-      window.localStorage.setItem("quiz-user", JSON.stringify(data));
-      // NOTE: we've stored the user globally (i.e. outside of
-      // this function)
-      user = data;
-      // Now we're ready to start the quiz
-      renderQuizPage();
-    }
-  } catch (err) {
-    // If something went wrong, we'll redirect the user to the login
-    // page
-    renderLoginPage();
-  }
+  let loginMain = document.querySelector("main");
+  let loginForm = loginMain.querySelector("form");
+  loginForm.addEventListener("submit", submitLogin);
 }

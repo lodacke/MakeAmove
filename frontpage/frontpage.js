4ab24e00deab320aac798d8c00f2330899ee3594
Grace <yintingchancytg@gmail.com>
2023-04-26 17:40:41 +0200

    Created nav and exported it to explore page

--- frontpage/frontpage.js
+++ frontpage/frontpage.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { renderDatingPage } from "../date/date.js";
+
 function checkIfUserLoggedIn() {
   if(window.localStorage.getItem("user")) {
     renderDatingPage();
---
+++ stickyNav/stickyNav.js
@@ -0,0 +1,11 @@
+export function stickyNav() {
+  return `
+    <nav class="sticky-nav">
+      <ul>
+        <li>Profile</li>
+        <li>Explore</li>
+        <li>Match</li>
+      </ul>
+    </nav>
+  `;
+}

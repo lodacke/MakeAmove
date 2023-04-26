d71613f24d844f9b61de064d5e1ffc6a4667086b
Grace <yintingchancytg@gmail.com>
2023-04-26 18:13:42 +0200

    Updated the structure of navbar

--- stickyNav/stickyNav.js
+++ stickyNav/stickyNav.js
@@ -2,9 +2,18 @@ export function stickyNav() {
   return `
     <nav class="sticky-nav">
       <ul>
-        <li>Profile</li>
-        <li>Explore</li>
-        <li>Match</li>
+        <li>
+          <img class="icon" src="../image/profile.png" alt="profile">
+          <p class="icon-name">Profile</p>
+        </li>
+        <li>
+          <img class="icon" src="../image/explore.png" alt="explore">
+          <p class="icon-name">Explore</p>
+        </li>
+        <li>
+          <img class="icon" src="../image/heart.png" alt="heart">
+          <p class="icon-name">Match</p>
+        </li>
       </ul>
     </nav>
   `;

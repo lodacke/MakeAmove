d628a3a6f205948513e68adada492ce83c7c9080
Grace <yintingchancytg@gmail.com>
2023-05-04 08:53:52 +0200

    Added some classes for the navbar's buttons

--- stickyNav/stickyNav.js
+++ stickyNav/stickyNav.js
@@ -2,15 +2,15 @@ export function stickyNav() {
   return `
     <nav class="sticky-nav">
       <ul>
-        <li>
+        <li class="profile page">
           <img class="icon" src="../image/profile.png" alt="profile">
           <p class="icon-name">Profile</p>
         </li>
-        <li>
+        <li class="explore page">
           <img class="icon" src="../image/explore.png" alt="explore">
           <p class="icon-name">Explore</p>
         </li>
-        <li>
+        <li class="match page">
           <img class="icon" src="../image/heart.png" alt="heart">
           <p class="icon-name">Match</p>
         </li>

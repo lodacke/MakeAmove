947bccc030a0aa71f592e1052bcaad74c60019c7
sofiesteier <steiersofie@gmail.com>
2023-05-10 12:15:23 +0200

    Update explore.php

--- PHP/date/explore.php
+++ PHP/date/explore.php
@@ -2,10 +2,32 @@
 
 ini_set("display_errors", 1);
 
-require_once("../PHP/helper.php");
+require_once("../helper.php");
+
+$filename = "../DB/users.json";
 
 if($_SERVER["REQUEST_METHOD"] === "GET"){
-    
+    $json = file_get_contents($filename);
+    $users = json_decode($json, true);
+
+    $requestInput = file_get_contents("php://input");
+    $requestDATA = json_decode($requestInput, true);
+
+    $email = $requestDATA["email"];
+
+    for($i = 0; $i < count($users); $i++){
+        $user = $users[$i];
+        if($user["email"] == $email)
+
+
+
+    if(in_array("", $users))
+
+    $randomIndex = array_rand($users);
+    $randomuserIndex = array_rand($users, 1);
+    $randomUser = array_rand($users, $randomIndex);
+
+
 }
 
 ?> 

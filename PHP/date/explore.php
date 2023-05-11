1931010d3aa56d0fae23f65bbfee4a797241834f
lodacke <lo.dacke@mail.com>
2023-05-11 13:07:27 +0200

    added to PHP of explore

--- PHP/date/explore.php
+++ PHP/date/explore.php
@@ -26,12 +26,31 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 
 // skapa json-fil för matchningar, bestående av email & ja/nej, en stor json fil för alla som sedan matchas ihop users.json för att visa matchningar.)
 
+    $json = file_get_contents($filename);
+    $users = json_decode($json, true);
+
+    $requestInput = file_get_contents("php://input");
+    $requestDATA = json_decode($requestInput, true);
+
+    $userEmail = $requestDATA["email"];
+    $preferenceGender = $requestDATA["preference"]["genderOf"];
+    $preferenceAgeMax = $requestDATA["preference"]["ageOfMax"];
+    $preferenceAgeMin = $requestDATA["preference"]["ageOfMin"];
+    $gender = $requestDATA["gender"];
+    $age = $requestDATA["age"];
+
+    $prefUser = [];
+
     forEach($users as $user){
         if($user["email"] != $userEmail){
              if($user["age"] >= $preferenceAgeMin && $user["age"] <= $preferenceAgeMax && $user["gender"] === $preferenceGender){
                 if($user["preference"]["ageOfMin"] <= $age && $user["preference"]["ageOfMax"] >= $age && $user["gender"] === $gender)
 
-                $prefUser[] = $user;          
+                $newUser = $user;
+                unset($newUser["interests"]["contact"]);
+                unset($newUser["password"]);
+                unset($newUser["preference"]);
+                $prefUser[] = $newUser;          
             }
         }
     }

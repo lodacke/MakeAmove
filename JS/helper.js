512947609bc1babed25e5be1878450f3f4a3acc0
Grace <yintingchancytg@gmail.com>
2023-05-23 18:56:20 +0200

    Updated error messages

--- JS/helper.js
+++ JS/helper.js
@@ -37,25 +37,25 @@ export function errorMessage(){
 
          switch(required.attributes.name.value){
             case "name":
-                required.placeholder = "You need to write you're name.";
+                required.placeholder = "Please enter your name";
                 break;
             case "email":
-                required.placeholder ="This does not look like an emailadress?";
+                required.placeholder = "This does not look like an email";
                 break;
             case "password":
-                required.placeholder = "The password needs to be atleast 8 characters.";
+                required.placeholder = "The password must be at least 8 characters";
                 break;
             case "age":
-                required.placeholder = "You need to be atleast 18 years old to use this app.";
+                required.placeholder = "You need to be at least 18 years old to use this app";
                 break;
             case "tel":
-                required.placeholder = "Phone number is needed!";
+                required.placeholder = "Phone number is needed";
                 break;
             case "ageOfMax":
-                required.placeholder = "Here you add the max age.";
+                required.placeholder = "Enter the preferred max age of your date";
                 break;
             case "ageOfMin":
-                required.placeholder = "Here you add the min age."
+                required.placeholder = "Enter the preferred min age of your date"
             }
     }  else {
             required.classList.remove("notAnswered");
